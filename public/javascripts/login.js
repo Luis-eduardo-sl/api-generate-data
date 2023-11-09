cf52e392d2fbfd65e2a336efa6d532feafdadfe5
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2023-11-09 16:45:10 -0300

    api

---
+++ public/javascripts/login.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const token = getCookie('token');
+    // const tokenInfo = decodeToken(token);
+    console.log(token);
+
+    const form = document.querySelector("#form");
+  
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+  
+      if (form.checkValidity()) {
+        const email = document.querySelector("#email").value;
+        const password = document.querySelector("#password").value;
+        const data = { email, password };
+  
+        try {
+          const response = await axios.post("http://localhost:5000/api/users/login", data);
+  
+          const token = response.data.accessToken;
+          setCookie('token', token);
+
+          // decodifica o token para acessar os dados do payload.
+          const tokenInfo = decodeToken(token);
+          console.log(tokenInfo);
+
+          // redireciona se necessário.
+          // window.location.href = `http://localhost:3000/`;
+        } catch (error) {
+          // tratar em caso de erro: mensagens, redireciona, etc.
+          console.log(error.message);
+        }
+      }
+
+    });
+});
